2211f9e8805e8f87348a4f09e3d5cac858f39ad9
Andreas Severin Nielsen <zbcannie43@zbc.dk>
2020-01-31 12:10:57 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODP_Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton ref1 = Singleton.Instance;
+            Singleton ref2 = Singleton.Instance;
+
+            Console.WriteLine("ref1: " + ref1.ToString());
+            Console.WriteLine("swapping the strings on ref2.");
+            ref2.Swap();
+            Console.WriteLine("ref1: " + ref1.ToString());
+            Console.WriteLine("ref1 and ref2 are the same: " + (ref1 == ref2).ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODP_Singleton
+{
+	public sealed class Singleton
+	{
+		// Only called when requested for the first time, and only run once
+		private static readonly Lazy<Singleton> lazy = new Lazy<Singleton>(() => new Singleton());
+
+		// Getting the object from the lazy value
+		public static Singleton Instance { get { return lazy.Value; } }
+
+		// private instantiation of the singleton
+		private Singleton()
+		{
+		}
+
+		private string s1 = "12345";
+
+		public string S1
+		{	
+			get { return lazy.Value.s1; }
+			set { lazy.Value.s1 = value; }
+		}
+
+		private string s2 = "67890";
+
+		public string S2
+		{
+			get { return lazy.Value.s2; }
+			set { lazy.Value.s2 = value; }
+		}
+
+		public void Swap()
+		{
+			string sTemp = S1;
+			S1 = S2;
+			S2 = sTemp;
+		}
+
+		public override string ToString()
+		{
+			return S1 + S2;
+		}
+	}
+}
